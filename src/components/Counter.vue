fd8ce4ce375ff28162a1b81d773dfc487a0af944
Ramón Montalbán <iphonederamon@icloud.com>
2023-09-07 23:27:41 +0200

    First commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'Counter',
+  data(){
+    return{
+      counter: 5
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>Counter</h2>
+  <p>{{counter}} <sup>2</sup></p>
+</template>
+
+<style scoped>
+
+</style>
