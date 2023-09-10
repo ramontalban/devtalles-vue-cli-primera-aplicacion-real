10f58c988a67a6a3228a93ee762e5258870da3e7
Ramón Montalbán <iphonederamon@icloud.com>
2023-09-10 12:02:28 +0200

    Final commit

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,19 +1,65 @@
 <script>
 export default {
+  props: {
+    title: String,
+    start: {
+      type: Number,
+      default: 5,
+      validator(value){
+        return value >= 0
+      }
+    }
+  },
   name: 'Counter',
   data(){
     return{
-      counter: 5
+      counter: this.start
+    }
+  },
+  methods: {
+    getSquareValue(){
+      return this.counter * this.counter
+    },
+    increase(){
+      this.counter++
+    },
+    decrease(){
+      this.counter--
+    }
+  },
+  computed: {
+    squareCounter(){
+      return this.counter * this.counter
+    },
+    customTitle(){
+      return this.title || 'Counter'
     }
   }
 }
 </script>
 
 <template>
-  <h2>Counter</h2>
-  <p>{{counter}} <sup>2</sup></p>
+  <h2>{{ customTitle }}</h2>
+  <p>{{counter}} <sup>2</sup> = {{ squareCounter }}</p>
+  <div>
+    <button v-on:click="increase">+ 1</button>
+    <button @click="decrease">- 1</button>
+  </div>
+
 </template>
 
-<style scoped>
-
+<style>
+  button{
+    background-color: #64BB87;
+    border-radius: 5px;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+    padding: 5px 15px;
+    transition: 0.3s esase-in-out;
+  }
+  button:hover{
+    background-color: #5AA67B;
+    transition: 0.3s esase-in-out;
+  }
 </style>
